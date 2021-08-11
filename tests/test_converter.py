70314446cc468dbb4f824d8b13f76d342b1e026e
Daniel Lopes <ddanielssoares@gmail.com>
2021-08-10 21:59:39 -0300

    Tests open image example

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -10,14 +10,17 @@ def test_should_return_jpeg_image_object(image_processing):
 
 @mark.usefixtures('image_processing')
 def test_should_return_image_with_new_size(image_processing):
+    image_processing.open_image('./example.jpg')
     assert image_processing.resize_image(100) == (100, 56)
 
 
 @mark.usefixtures('image_processing')
 def test_should_return_image_gray_scale(image_processing):
+    image_processing.open_image('./example.jpg')
     assert type(image_processing.grayify()) is JpegImageFile
 
 
 @mark.usefixtures('example_image')
 def test_should_return_image_string(image_processing):
+    image_processing.open_image('./example.jpg')
     assert type(image_processing.pixels_to_ascii()) is str
